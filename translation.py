4386f352547d4f0fd658a54285bf709af7221f38
sampathsir <83348785+sampathsir@users.noreply.github.com>
2021-06-03 19:20:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -13,7 +13,7 @@ if you do not trust this bot dev, please host this bot yourself
 by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
 
 /start at any stage to re-enter your details"""
-    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
+    BEFORE_SUCC_LOGIN = "recieved code. getting your details ..."
     ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
