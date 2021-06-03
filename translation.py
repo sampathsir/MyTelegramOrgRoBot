86f9310b98d6e3235f991a916f76c7937cba7a58
sampathsir <83348785+sampathsir@users.noreply.github.com>
2021-06-03 19:24:49 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -17,4 +17,8 @@ by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pi
     ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number, Please send your phone number"
+    IN_VALID_PHNO_PVDED = "Hi, Bʀᴏ I can get your Telegram APP ID & API Hash from my.telegram.org !
+No need to go to website & login there. Just Login with me
+Just Send me your Telegram Account's Phone Number to start process.
+Phone Number should be in International Format.
+Example: +8805748292"
